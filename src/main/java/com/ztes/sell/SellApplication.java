c496e0b75928e0cc000ba98330f7af1c1186af50
muling105 <muling105@aliyun.com>
2018-06-08 10:28:37 +0800

    项目打war包修改主类 该类继承了SpringBootServletInitializer并且重写了configure方法

--- src/main/java/com/ztes/sell/SellApplication.java
+++ src/main/java/com/ztes/sell/SellApplication.java
@@ -2,13 +2,24 @@ package com.ztes.sell;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+/**
+ * 如果使用war包则要继承SpringBootServletInitializer并且重写configure方法，见注释
+ */
 @SpringBootApplication
 @EnableTransactionManagement
 public class SellApplication {
+//public class SellApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SellApplication.class, args);
 	}
+
+	/*@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SellApplication.class);
+    }*/
 }
--- src/main/java/com/ztes/sell/config/WebSocketConfig.java
+++ src/main/java/com/ztes/sell/config/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.ztes.sell.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
